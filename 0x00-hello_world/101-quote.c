32f7951069405e6a7f67a319a5a0e0a7782a28f3
Emmanuel Akins <Akins1412>
2023-08-10 17:12:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *
  * main - Entry point
  *
  * Return: Always 1 (Success)
@@ -10,4 +9,4 @@ int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
 	return (1);
- }
+}
