db7bee159bcc81ea42722ef8be5484e3312d1989
Emmanuel Akins <Akins1412>
2023-08-11 18:51:28 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch 'a';
+	int CH 'A';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (ch <= 'A')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n')
+	return (0);
+}
