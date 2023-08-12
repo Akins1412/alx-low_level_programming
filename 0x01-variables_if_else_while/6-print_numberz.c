831923b3d6a9ae81dbd35060eca39418da7ad0cb
Emmanuel Akins <Akins1412>
2023-08-12 03:10:08 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,15 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+
+int main(void)
 {
 	int n;
-	for (n = '0'; n <= '9'; n++)
+
+	for  (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
 
 	putchar('\n');
-
 	return (0);
 }
