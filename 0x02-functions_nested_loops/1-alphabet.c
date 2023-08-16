5b752caa188429cd1cd8de9987e374a43552ccf4
Emmanuel Akins <Akins1412>
2023-08-15 20:15:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; c++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
