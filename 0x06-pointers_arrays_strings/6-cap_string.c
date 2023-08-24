4193445591b9ce5254b5b57962ff3240705b2ab6
Emmanuel Akins <Akins1412>
2023-08-23 20:11:41 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizing all words of a string
+ * @str: The string should be capitalized
+ *
+ * Return: A pointer to the chnaged string.
+ */
+char *cap_string(char *str)
+{
+	int index = o;
+
+	while (str[index])
+	{
+		while (!str[index] >= 'a' && str[index] <= 'z')
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+	return (str);
+}
