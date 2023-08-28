791f4e5831134ba7d02e5a2bfccd9c5de80f0ab4
Emmanuel Akins <Akins1412>
2023-08-28 10:03:22 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @n: number of bytes to be changed
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ *
+ * Return: success (s)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
