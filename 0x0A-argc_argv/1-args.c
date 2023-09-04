be71a8c5e7e87186d9bda424a3cb20e24b1ebb91
Emmanuel Akins <Akins1412>
2023-09-03 18:50:36 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - print its number
+ * @argc: count number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
